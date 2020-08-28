d82287c6fc7f43e350b8e5839b1d5bc06d8e7c34
Moritz <moritzlauff@t-online.de>
2020-08-28 13:46:27 +0200

    callback for mse added

---
+++ python/functions/training_callback.py
@@ -0,0 +1,24 @@
+# Callbacks for monitoring the accuracy and MSE after each iteration
+# classes:
+#   BatchAccuracy
+#   BatchMSE
+
+import tensorflow
+
+class BatchAccuracy(tensorflow.python.keras.callbacks.Callback):
+    def __init__(self, test_data):
+        self.test_data = test_data
+        self.acc = []
+
+    def on_batch_end(self, epoch, logs = {}):
+        X, y = self.test_data
+        self.acc.append(self.model.evaluate(X, y, verbose = 0)[1])
+
+class BatchMSE(tensorflow.python.keras.callbacks.Callback):
+    def __init__(self, test_data):
+        self.test_data = test_data
+        self.acc = []
+
+    def on_batch_end(self, epoch, logs = {}):
+        X, y = self.test_data
+        self.acc.append(self.model.evaluate(X, y, verbose = 0)[1])
